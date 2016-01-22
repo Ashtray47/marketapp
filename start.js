68ff30c705cc43a363c6e93d0b7ae57397fc3f12
ashtray47 <msau01@yahoo.com>
2016-01-21 21:05:24 -0800

    add new files

--- start.js
+++ start.js
@@ -2,27 +2,26 @@
 'use strict';
 
 var express = require('express');
-
 var app = express();
-
 var parser = require('body-parser');
 
 var marketListDB = require('./server/items.js');
 
-var port = 5000;
+app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static('dist'));
+app.use(express.static(__dirname + '/dist'));
 
-app.listen(port, function(err) {
-	console.log('running server on port ' + port);
-})
+// views is directory for all template files
+app.set('views', __dirname + '/dist');
+app.set('view engine', 'html');
 
-app.get('/', function(req, res) {
-	res.render('index');
-	//res.sendFile('/index.html');
+app.get('/', function(request, response) {
+  response.render('index');
 });
 
-
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
 
